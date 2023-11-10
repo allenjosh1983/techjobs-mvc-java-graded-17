5799dd773dfc36eeec57fb6a13c277e87b2cdee7
allenjosh1983 <138170235+allenjosh1983@users.noreply.github.com>
2023-11-09 20:58:22 -0600

    display search results

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,18 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
+import org.launchcode.techjobsmvc.models.JobData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -15,6 +24,7 @@ import static org.launchcode.techjobsmvc.controllers.ListController.columnChoice
 @RequestMapping("search")
 public class SearchController {
 
+
     @GetMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
@@ -22,6 +32,19 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping(value = "results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<Job> jobs;
+        if (searchTerm.equals("all") || searchTerm.equals("")) {
+        jobs = JobData.findAll();
+    } else {
+        jobs = JobData.findByColumnAndValue(searchType, searchTerm.toLowerCase());
+    }
 
-}
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columnChoices", ListController.columnChoices);
+        model.addAttribute("title"+ ListController.columnChoices.get(searchTerm));
+        return "search";
+}}
+
 
